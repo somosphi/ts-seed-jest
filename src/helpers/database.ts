87a2b2a1734bc30ad207819340e08011506a0039
Ariel Ril <arielril@Ariels-MacBook-Pro.local>
2019-10-22 12:44:58 -0300

    fix databse helper file

--- src/helpers/database.ts
+++ src/helpers/database.ts
@@ -1,6 +1,6 @@
 import knex from 'knex';
 
-import knexfile from '../../knexfile';
+import knexfile = require('../../knexfile');
 
 export default function database(connector = 'mysql') {
   switch (connector) {
